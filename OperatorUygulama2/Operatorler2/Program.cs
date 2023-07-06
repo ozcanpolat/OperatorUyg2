b1c488e12136215820914db9f51d79d901b3e9c6
ozcanpolat <93453642+ozcanpolat@users.noreply.github.com>
2023-07-06 13:51:01 +0300

    OperatorUyg2

---
+++ OperatorUygulama2/Operatorler2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Operatorler2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // (x, y, z) toplamının mod 3' ü kaçtır?
+            int x = 10, y = 20, z = 27;
+
+            int sonuc = (x + y + z) % 3;
+
+            Console.WriteLine("Sonuç :" + sonuc);
+            
+        }
+    }
+}
